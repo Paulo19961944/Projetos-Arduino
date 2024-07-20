dae504d44570d74a0b97083d85e6e0d2504857ec
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-07-20 20:03:22 -0300

    Create 05-Controle-Remoto.cpp

---
+++ 05-Controle-Remoto.cpp
@@ -0,0 +1,22 @@
+#include <IRremote.hpp>
+#define IR_RECEIVE_PIN 2
+#define LED 7
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+      long numControl = IrReceiver.decodedIRData.decodedRawData;
+      Serial.println(numControl); // Print "old" raw data
+      IrReceiver.resume(); // Enable receiving of the next value
+    
+    if(numControl == -16728320){
+      digitalWrite(LED, HIGH);
+    }
+  } 
+}
