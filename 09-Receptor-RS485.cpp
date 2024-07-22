6eaef7fcf99b3f5a9d23dfd7e97a6370eade646f
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-07-22 10:30:37 -0300

    Create 09-Receptor-RS485.cpp

---
+++ 09-Receptor-RS485.cpp
@@ -0,0 +1,64 @@
+#include <SoftwareSerial.h>
+
+unsigned int tempoAnterior = 0; // Tempo da última impressão
+
+// Pinos de comunicação serial do módulo RS485
+#define Pino_RS485_RX 10 // DO (Direct Output)
+#define Pino_RS485_TX 11 // DI (Direct Input)
+
+// Pino de controle transmissão/recepção
+#define SSerialTxControl 3
+#define RS485Transmit HIGH
+#define RS485Receive LOW
+
+// Cria a serial por software para conexão com módulo RS485
+SoftwareSerial RS485Serial(Pino_RS485_RX, Pino_RS485_TX);
+
+// Armazena os dados que chegam pela serial
+String inputString = "";
+
+void setup() {
+  // Inicializa a serial do Arduino
+  Serial.begin(9600);
+  Serial.println("Módulo Receptor");
+  Serial.println("Aguardando dados...");
+  pinMode(SSerialTxControl, OUTPUT);
+
+  // Coloca o módulo RS485 em modo de recepção
+  digitalWrite(SSerialTxControl, RS485Receive);
+
+  // Inicializa a serial do módulo RS485
+  RS485Serial.begin(4800);
+}
+
+void loop() {
+  // Recebe os dados do RS485 via porta serial
+  if (RS485Serial.available()) {
+    while (RS485Serial.available()) {
+      // Recebe os dados e monta a string
+      char inChar = (char)RS485Serial.read();
+      inputString += inChar;
+
+      // Se encontrou o caractere de nova linha, processa a string
+      if (inChar == '\n') {
+        bufferPrint(inputString, tempoAnterior);
+        inputString = ""; // Limpa a string para receber novos dados
+      }
+    }
+  }
+}
+
+void bufferPrint(String codigo, unsigned int leituraAnterior) {
+  // Verifica se passaram 500 ms desde a última impressão
+  if (millis() - tempoAnterior >= leituraAnterior && codigo.length() > 0) {
+    // Encontra a posição do primeiro caractere de vírgula
+    int posVirgula = codigo.indexOf(',');
+    String resultado = codigo.substring(1, posVirgula);
+    if (posVirgula != -1) {
+      
+      // Imprime a String conforme a lógica desejada
+      Serial.println(resultado);
+      tempoAnterior = millis(); // Atualiza o tempo anterior para o atual
+    }
+  }
+}
