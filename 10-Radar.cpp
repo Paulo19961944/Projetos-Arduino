4daaf9cdd6c443f5d2c44a2088ee0dd41da78004
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-07-28 19:45:23 -0300

    Update 10-Radar.cpp

--- 10-Radar.cpp
+++ 10-Radar.cpp
@@ -5,20 +5,26 @@ SoftwareSerial sensorSerial(10, 11); // RX, TX
 
 void setup() {
   // Inicialize a comunicação serial com o monitor serial a 9600 bps
-  Serial.begin(9600);     
+  Serial.begin(9600);
   
   // Inicialize a comunicação serial com o sensor a 256000 bps
-  sensorSerial.begin(256000);  
+  sensorSerial.begin(256000);
 }
 
 void loop() {
   // Verifique se há dados disponíveis do sensor
   if (sensorSerial.available()) {
-    // Leia um byte de dados do sensor
     byte data = sensorSerial.read();
     
-    // Imprima o valor recebido em formato decimal
-    if(data < 1){
+    // Imprime o valor recebido em decimal e hexadecimal
+    Serial.print("Dados recebidos (Decimal): ");
+    Serial.print(data);
+    Serial.print("  (Hexadecimal): ");
+    Serial.println(data, HEX);
+    
+    // Exemplo de lógica para detectar valores específicos
+    int detectionThreshold = 50; // Ajuste conforme necessário
+    if (data < detectionThreshold) {
       Serial.println("Pessoa Detectada!");
     }
   }
